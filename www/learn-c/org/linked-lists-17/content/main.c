da40d63a5934dfb13595feabf881c8b880a38dff
Evanlab02 <evanlabuschagne70@gmail.com>
2024-05-19 09:42:13 +0200

    chore: WIP Linked Lists Content

---
+++ www/learn-c/org/linked-lists-17/content/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structs
+typedef struct node {
+  int val;
+  struct node * next;
+} node_t;
+
+// Function declarations
+void print_list(node_t * head);
+void push_end(node_t * head, int val);
+void push(node_t ** head, int val);
+int pop(node_t ** head);
+
+// Entrypoint
+int main() {
+  // create head and allocate memory for it.
+  node_t * head = NULL;
+  head = (node_t *) malloc(sizeof(node_t));
+  
+  // If it is still NULL, exit with code 1.
+  if (head == NULL) {
+    return 1;
+  }
+
+  // Assign head intial value.
+  head->val = 1;
+  head->next = NULL;
+
+  // Add more items to linked list
+  // START
+  head->val = 1;
+  head->next = (node_t *) malloc(sizeof(node_t));
+  
+  // Check that allocation did not fail...
+  if (head->next == NULL) {
+    return 1;
+  }
+
+  head->next->val = 2;
+  head->next->next = NULL;
+  // END
+
+  // Print list after initialization
+  printf("-----\n");
+  print_list(head);
+  printf("-----\n");
+
+  // Push new node to the end of the linked list
+  push_end(head, 3);
+
+  // Print list after pushing to it
+  printf("-----\n");
+  print_list(head);
+  printf("-----\n");
+
+  // Push new node to start of the the linked list
+  push(&head, 4);
+
+  // Print list after pushing to it
+  printf("-----\n");
+  print_list(head);
+  printf("-----\n");
+
+  // Pop the first item from the linked list.
+  pop(&head);
+
+  // Print list after pushing to it
+  printf("-----\n");
+  print_list(head);
+  printf("-----\n");
+
+  // Return exit code 0
+  return 0;
+}
+
+// Print the list of nodes
+void print_list(node_t * head) {
+  node_t * current = head;
+
+  while (current != NULL) {
+    printf("%d\n", current->val);
+    current = current->next;
+  }
+}
+
+// Add to end of linked list
+void push_end(node_t * head, int val) {
+  node_t * current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  /* now we can add a new variable */
+  current->next = (node_t *) malloc(sizeof(node_t));
+  current->next->val = val;
+  current->next->next = NULL;
+}
+
+// Add to start of linked list
+void push(node_t ** head, int val) {
+  node_t * new_node;
+  new_node = (node_t *) malloc(sizeof(node_t));
+
+  new_node->val = val;
+  new_node->next = *head;
+  *head = new_node;
+}
+
+// Remove the item at the start of the linked list
+int pop(node_t ** head) {
+  int retval = -1;
+  node_t * next_node = NULL;
+
+  if (*head == NULL) {
+    return -1;
+  }
+
+  next_node = (*head)->next;
+  retval = (*head)->val;
+  free(*head);
+  *head = next_node;
+
+  return retval;
+}
+
